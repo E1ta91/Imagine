88d23795430f1004929b755399b40ef9fd395452
Sebiyam Elsie Tani <elsietani117@gmail.com>
2024-06-06 15:58:52 +0000

    Math in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -73,7 +73,47 @@ const tweet1 = {
     console.log('notify friends!', i);
   }
   
-  
+  // Functions
+// Defining functions
+function login(username, password) {
+  if(username == 'elsie' && password =='1234') {
+     return 'User is logged in!';
+  }  else if (username!='elsie') {
+    return 'Invalid username'
+  } else if(password!=1234) {
+    return 'Invalid password'
+  }  else {
+    return 'Invalid username or password';
+  }
+ }
+ //  Invoking functions
+ login('elsie', '1234');
+ 
+ // Basic Arithmetic
+ 12 + 34;
+ 45 - 16;
+ 7  * 12;
+ 78 / 4;
+ 12 % 5
+ 
+ // Comparison operators
+ 2 === 2;
+ 3 !== 2;
+ 3 !== 3;
+ 
+ // Built-in Math Functions
+ Math.round(37/24);
+ Math.floor(37/24) ;
+ Math.ceil(37/24) ;
+ Math.random();
+ Math.max(78, 12, 98, 14, 89, 13 );
+ 
+ // Order of operations
+ 1 * 2 + 3 - 4 / 2 + 2**2;  
+ 
+ // Handling user inputs
+ 1200+ Number('250')
+ 
   
   
   
